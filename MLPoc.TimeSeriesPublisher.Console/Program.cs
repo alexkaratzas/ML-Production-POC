ca035361ec4ca79fd7ce60fd7242e26f1f524c0d
Alex Karatzas <alexk@digiterre.com>
2018-03-01 15:27:16 +0000

    Publish first message on Kafka queue

---
+++ MLPoc.TimeSeriesPublisher.Console/Program.cs
@@ -0,0 +1,15 @@
+﻿
+namespace MLPoc.TimeSeriesPublisher.Console
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            System.Console.WriteLine("Starting up Time Series Publisher...");
+
+            var publisher = new KafkaPublisher();
+
+            publisher.Publish();
+        }
+    }
+}
