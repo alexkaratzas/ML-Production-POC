28f6c9a65e662215f51345a534a7c0a245265d88
Alex Karatzas <alexk@digiterre.com>
2018-03-02 10:14:28 +0000

    Added time series processing projects

---
+++ MLPoc.FeatureExtractor.Console/Program.cs
@@ -0,0 +1,11 @@
+﻿
+namespace MLPoc.FeatureExtractor.Console
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+        }
+    }
+}
